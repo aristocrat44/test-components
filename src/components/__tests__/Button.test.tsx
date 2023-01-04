ae22d2adf981e0c5831671e936c87783040a304d
Atish Maharjan <atish@opencafe.io>
2023-01-04 14:22:31 +0545

    add testing library

---
+++ src/components/__tests__/Button.test.tsx
@@ -0,0 +1,11 @@
+import { describe, expect, test } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { Button } from '../Button';
+
+describe('Accordion test', () => {
+  test('should show title all the time', () => {
+    render(<Button label="hello" />);
+
+    expect(screen.getByText(/hello/i)).toBeDefined();
+  });
+});
